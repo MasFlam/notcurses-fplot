478669d49c395c047a12c947d04ba8865366f1ab
MasFlam <masflam@masflam.com>
2021-06-02 16:47:29 +0200

    Set stock config to sin()

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 #include <math.h>
 #include "functions.h"
 
-double (*g_function)(double) = log2;
-double g_xstart = 1;
-double g_xend = 1e5;
+double (*g_function)(double) = sin;
+double g_xstart = -2*M_PI;
+double g_xend = 2*M_PI;
